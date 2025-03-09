10514951872b2071c96ed1e3d7fdb6ddb2ae23f9
Ziruax <120768025+Ziruax@users.noreply.github.com>
2025-03-09 17:49:43 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-pip install -r requirements.txt
-python -m spacy download en_core_web_sm --no-deps
+
+# Upgrade pip to the latest version
+python -m pip install --upgrade pip
+
+# Install uv if not already present, then upgrade it
+pip install --upgrade uv
+
+# Install dependencies from requirements.txt using uv pip
+uv pip install -r requirements.txt
